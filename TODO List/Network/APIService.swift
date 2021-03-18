63509c87eb0a33c3f37e441a9ea2a332ae6dc06a
Klyukay Evangelina <klyukay.evangelina@gmail.com>
2021-03-18 13:34:33 +0300

    Network

--- TODO List/Network/APIService.swift
+++ TODO List/Network/APIService.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+
+class APIServise {
+    
+    let file = "DataBase.json"
+
+    func loadJson(filename fileName: String) -> [Task]? {
+        if let url = Bundle.main.url(forResource: file, withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(TaskData.self, from: data)
+                return jsonData.task
+            } catch {
+                print("error:\(error)")
+            }
+        }
+        return nil
+    }
+    
+}
